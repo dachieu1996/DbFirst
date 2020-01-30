67ec69242d1e2c79d0520bdcd96721671a9afc56
Nguyễn Đắc Hiếu <dachieu1996@gmail.com>
2020-01-30 13:22:03 +0700

    add console .Net Framework

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DbFirst
 {
@@ -6,7 +10,6 @@ namespace DbFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
